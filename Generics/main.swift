3168f9958965a4127af1ed0e42e7df418454e78f
Temich93s <nil>
2021-10-09 15:19:46 +1000

    Пройдена тема: Проблема, которую решают универсальные шаблоны

--- Generics/main.swift
+++ Generics/main.swift
@@ -9,3 +9,42 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Проблема, которую решают универсальные шаблоны
+print("\n//Проблема, которую решают универсальные шаблоны")
+
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+// Выведет "someInt is now 107, and anotherInt is now 3"
+
+func swapTwoStrings(_ a: inout String, _ b: inout String) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someString = "+3"
+var anotherString = "+107"
+swapTwoStrings(&someString, &anotherString)
+print("someInt is now \(someString), and anotherInt is now \(anotherString)")
+
+func swapTwoDoubles(_ a: inout Double, _ b: inout Double) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someDouble = 3.0
+var anotherDouble = 107.0
+swapTwoDoubles(&someDouble, &anotherDouble)
+print("someInt is now \(someDouble), and anotherInt is now \(anotherDouble)")
+
+
