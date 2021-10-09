f872d0d4746f48efdddc324b745ad853f7aab2a2
Temich93s <nil>
2021-10-09 18:13:20 +1000

    Пройдена тема: Ограничение типа в действии

--- Generics/main.swift
+++ Generics/main.swift
@@ -123,3 +123,38 @@ if let topItem = stackOfStrings.topItem {
     print("The top item on the stack is: \(topItem).")
 }
 // Выведет "The top item on the stack is tres."
+
+
+//MARK: Ограничение типа в действии
+print("\n//Ограничение типа в действии")
+
+func findIndex(ofString valueToFind: String, in array: [String]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+let strings = ["cat", "dog", "llama", "parakeet", "terrapin"]
+if let foundIndex = findIndex(ofString: "llama", in: strings) {
+    print("The index of llama is \(foundIndex)")
+}
+// Выведет "The index of llama is 2"
+
+func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
+print(doubleIndex as Any)
+// doubleIndex опциональный Int не имеющий значения, потому что значения 9.3 нет в массиве
+let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
+print(stringIndex as Any)
+// stringIndex опциональный Int равный 2
